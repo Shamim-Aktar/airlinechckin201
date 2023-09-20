f4d20521753014b9415ab1b5af4194d0037dcdf8
Shamim-Akhtar <dakhtar81@gmail.com>
2023-09-20 23:02:48 +0530

    filter is working

--- src/flight/FlightDetails.js
+++ src/flight/FlightDetails.js
@@ -2,7 +2,7 @@ import React from 'react';
 import axios from 'axios';
 import { useState, useEffect } from 'react';
 import {
-     Select, MenuItem, FormControl, InputLabel, Grid
+    Select, MenuItem, FormControl, InputLabel, Grid
 } from "@mui/material";
 import moment from 'moment'
 
@@ -14,11 +14,11 @@ import PassengerList from '../passenger/PassengerList';
 const FlightDetails = () => {
 
     const [flightDetail, setFlightDetail] = useState([]);
-  // const [selectedValue, setSelectedValue]=useState([])
- 
-   const [flightId, setFlightId]=useState("")
-   const [show, setShow]=useState(false)
-  // const [singlePassenger, setSinglePassenger]=useState([])
+    // const [selectedValue, setSelectedValue]=useState([])
+
+    const [flightId, setFlightId] = useState("")
+    // const [show, setShow] = useState(false)
+    // const [singlePassenger, setSinglePassenger]=useState([])
 
 
     const fetchFlightDetail = async () => {
@@ -34,58 +34,58 @@ const FlightDetails = () => {
 
     useEffect(() => {
         fetchFlightDetail()
-    }, [] )
+    }, [])
 
     const handleChangeEvent = (event) => {
         console.log(event.target.value)
         // const listUrl = 'http://localhost:4000/PassengerDetailData'
         // const response = await axios.get(`${listUrl}?flight_id=${event.target.value}`)
         // console.log(response)
-        let flag=!show
-        setFlightId(event.target.value)  
-        setShow(flag)
+
+        setFlightId(event.target.value)
+
     };
 
 
     return (
 
         <div>
-               <Grid
-        style={{ height: "100%", padding: "12px" }}
-        container
-        direction="column"
-        justify="space-between"
-      >
-        <Grid item><FormControl  variant="outlined"margin={"normal"}>
-            <InputLabel id="test-select-label" style={{top: '2.2em'}}>Flight Details</InputLabel>
-            <Select
-                sx={{marginTop: 5, width: 250,height: 50 }}
-                onChange={handleChangeEvent}
-                label='Flight Details'
-                variant='outlined'
-                value={flightId}
+            <Grid
+                style={{ height: "100%", padding: "12px" }}
+                container
+                direction="column"
+                justify="space-between"
             >
-                {flightDetail && flightDetail.map((row, i)=>{
+                <Grid item><FormControl variant="outlined" margin={"normal"}>
+                    <InputLabel id="test-select-label" style={{ top: '2.2em' }}>Flight Details</InputLabel>
+                    <Select
+                        sx={{ marginTop: 5, width: 250, height: 50 }}
+                        onChange={handleChangeEvent}
+                        label='Flight Details'
+                        variant='outlined'
+                        value={flightId}
+                    >
+                        {flightDetail && flightDetail.map((row, i) => {
 
-            
-                console.log(row);
-                return(
-                    <MenuItem key={row.flight_id} value={row.flight_id}>
-                            {`${row.flightNo} ${row.fromPlace} - ${row.toPlace}
+
+                            console.log(row);
+                            return (
+                                <MenuItem key={row.flight_id} value={row.flight_id}>
+                                    {`${row.flightNo} ${row.fromPlace} - ${row.toPlace}
                                 ${moment(row.departureDate).format("ddd MMM Do YYYY h:mm a")} 
                                 ${moment(row.arrivalDate).format("ddd MMM Do YYYY h:mm a")}`
-                            }
-                          
-                      
-                    </MenuItem>
-                )
-                })}
-            </Select>
-            </FormControl>
-                </Grid>
-                </Grid>
+                                    }
 
-                {show && <PassengerList flightId={flightId}/>}
+
+                                </MenuItem>
+                            )
+                        })}
+                    </Select>
+                </FormControl>
+                </Grid>
+            </Grid>
+
+            {!!flightId && <PassengerList flightId={flightId} />}
         </div>
     );
 };
--- src/passenger/PassengerList.js
+++ src/passenger/PassengerList.js
@@ -1,77 +1,79 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios'
-import {Table, TableBody, TableCell, TableContainer, 
-    TableHead, TableRow, Paper, Grid} from "@mui/material";
-    import './Passengerlist.css'
+import {
+  Table, TableBody, TableCell, TableContainer,
+  TableHead, TableRow, Paper, Grid
+} from "@mui/material";
+import './Passengerlist.css'
 
 
-const PassengerList = ({flightId}) => {
-    console.log(flightId)
-    const[passengerlist, setPassengerList]=useState([])
+const PassengerList = ({ flightId }) => {
+  console.log(flightId)
+  const [passengerlist, setPassengerList] = useState([])
 
 
 
-useEffect(() => {
-    const fetchPassengerList= async ()=>{
-        const listUrl = 'http://localhost:4000/PassengerDetailData'
-        console.log(flightId)
-       // const response = await axios.get(`${listUrl}`)
-       const response = await axios.get(`${listUrl}?flight_id=${flightId}`)
+  useEffect(() => {
+    const fetchPassengerList = async () => {
+      const listUrl = 'http://localhost:4000/PassengerDetailData'
+      console.log(flightId)
+      // const response = await axios.get(`${listUrl}`)
+      const response = await axios.get(`${listUrl}?flight_id=${flightId}`)
         .catch((error) => {
-            console.log('Err', error)
+          console.log('Err', error)
         })
-            console.log(response.data)
-            setPassengerList(response.data)
+      console.log(response.data)
+      setPassengerList(response.data)
     }
     fetchPassengerList()
-}, [])
+  }, [flightId])
 
-    return (
-        <div className='passenger-list'>
-             <Grid item xs={12}>
-                 <TableContainer component={Paper}>
-      <Table aria-label="simple table" stickyHeader>
-        <TableHead >
-          <TableRow >
-            <TableCell align="center">First Name</TableCell>
-            <TableCell align="center">Last Name</TableCell>
-            <TableCell align="center">Mobile number</TableCell>
-            <TableCell align="center">Gender</TableCell>
-            <TableCell align="center">Infant</TableCell>
-            <TableCell align="center">Wheelchair</TableCell>
-            <TableCell align="center">Status</TableCell>
-            <TableCell align="center">Seat number</TableCell>
-            <TableCell align="center">DOB</TableCell>
-            <TableCell align="center">Passport</TableCell>
-            <TableCell align="center">Address</TableCell>
-            <TableCell align="center">Special meal</TableCell>
-            <TableCell align="center">Ancillary</TableCell>
+  return (
+    <div className='passenger-list'>
+      <Grid item xs={12}>
+        <TableContainer component={Paper}>
+          <Table aria-label="simple table" stickyHeader>
+            <TableHead >
+              <TableRow >
+                <TableCell align="center">First Name</TableCell>
+                <TableCell align="center">Last Name</TableCell>
+                <TableCell align="center">Mobile number</TableCell>
+                <TableCell align="center">Gender</TableCell>
+                <TableCell align="center">Infant</TableCell>
+                <TableCell align="center">Wheelchair</TableCell>
+                <TableCell align="center">Status</TableCell>
+                <TableCell align="center">Seat number</TableCell>
+                <TableCell align="center">DOB</TableCell>
+                <TableCell align="center">Passport</TableCell>
+                <TableCell align="center">Address</TableCell>
+                <TableCell align="center">Special meal</TableCell>
+                <TableCell align="center">Ancillary</TableCell>
 
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {passengerlist.map((row,i) => (
-            <TableRow key={i}>
-              <TableCell align="center">{row.first_name}</TableCell>
-              <TableCell align="center">{row.last_name}</TableCell>
-              <TableCell align="center">{row.mobile_no}</TableCell>
-              <TableCell align="center">{row.gender}</TableCell>
-              <TableCell align="center">{row.infant}</TableCell>
-              <TableCell align="center">{row.wheelChair}</TableCell>
-              <TableCell align="center">{row.seat_no}</TableCell>
-              <TableCell align="center">{row.passport}</TableCell>
-              <TableCell align="center">{row.address}</TableCell>
-              <TableCell align="center">{row.special_meals}</TableCell>
-              <TableCell align="center">{row.ancillary}</TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
-    </Grid>
-            
-        </div>
-    );
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {passengerlist.map((row, i) => (
+                <TableRow key={i}>
+                  <TableCell align="center">{row.first_name}</TableCell>
+                  <TableCell align="center">{row.last_name}</TableCell>
+                  <TableCell align="center">{row.mobile_no}</TableCell>
+                  <TableCell align="center">{row.gender}</TableCell>
+                  <TableCell align="center">{row.infant}</TableCell>
+                  <TableCell align="center">{row.wheelChair}</TableCell>
+                  <TableCell align="center">{row.seat_no}</TableCell>
+                  <TableCell align="center">{row.passport}</TableCell>
+                  <TableCell align="center">{row.address}</TableCell>
+                  <TableCell align="center">{row.special_meals}</TableCell>
+                  <TableCell align="center">{row.ancillary}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Grid>
+
+    </div>
+  );
 };
 
 export default PassengerList;
